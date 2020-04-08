33bcd2a3ed4789d0250af4212cb19d2379e130fc
Tim Cross <theophilusx@gmail.com>
2020-04-08 17:48:45 +1000

    Add details for a and img components.

--- demo/yorick_demo/main.cljs
+++ demo/yorick_demo/main.cljs
@@ -1,6 +1,10 @@
 (ns yorick-demo.main
   (:require [goog.dom :as gdom]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            [theophilusx.yorick.sidebar :as sb]
+            [theophilusx.yorick.utils :as utils]
+            [theophilusx.yorick.store :as store]
+            [yorick-demo.basic :refer [basic-page]]))
 
 (println "Reloading code")
 
@@ -10,6 +14,56 @@
 (defn mount [el component]
   (rdom/render component el))
 
+(def demo-sidebar {:sid :ui.menu
+                   :default-link :basic
+                   :data (sb/defsidebar-item
+                           :type :menu
+                           :title "Components"
+                           :items [(sb/defsidebar-item
+                                     :title "Basic"
+                                     :id :basic)
+                                   (sb/defsidebar-item
+                                     :title "Cards"
+                                     :id :cards)
+                                   (sb/defsidebar-item
+                                     :title "Icons"
+                                     :id :icons)
+                                   (sb/defsidebar-item
+                                     :title "Input"
+                                     :id :input)
+                                   (sb/defsidebar-item
+                                     :title "Media"
+                                     :id :media)
+                                   (sb/defsidebar-item
+                                     :title "Modal"
+                                     :id :modal)
+                                   (sb/defsidebar-item
+                                     :title "Navbar"
+                                     :item :navbar)
+                                   (sb/defsidebar-item
+                                     :title "Paginate"
+                                     :id :paginate)
+                                   (sb/defsidebar-item
+                                     :title "Sidebar"
+                                     :id :sidebar)
+                                   (sb/defsidebar-item
+                                     :title "Tables"
+                                     :id :tables)
+                                   (sb/defsidebar-item
+                                     :title "Toolbar"
+                                     :id :toolbar)])})
+
+(defn placeholder-page []
+  [:div.columns
+   [:div.column
+    [:h2.title.is-2 "Placeholder Page"]
+    [:p "This is the placeholder page for "
+     [:strong (str (store/get-in store/global-state (utils/spath :ui.menu)))]]]])
+
+(defn current-page []
+  (case (store/get-in store/global-state (utils/spath :ui.menu))
+    :basic [basic-page]
+    [placeholder-page]))
 
 (defn greeting []
   [:<>
@@ -19,7 +73,14 @@
       [:h1.title.is-1.has-text-white "Yorick Examples"]
       [:h3.subtitle.is-3
        "Example of " [:strong "Reagent"] " components styled with "
-       [:strong "Bulma"]]]]]])
+       [:strong "Bulma"]]]]]
+   [:section.section
+    [:div.container
+     [:div.columns
+      [:div.column.is-2
+       [sb/sidebar demo-sidebar]]
+      [:div.column
+       [current-page]]]]]])
 
 
 (defn mount-app []

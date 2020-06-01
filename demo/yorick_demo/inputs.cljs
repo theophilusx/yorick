c24042aa244815d0f235a31a7cbbc83ad50afd7e
Tim Cross <theophilusx@gmail.com>
2020-06-01 18:40:59 +1000

    Add examples of input component to demo

--- demo/yorick_demo/inputs.cljs
+++ demo/yorick_demo/inputs.cljs
@@ -1,6 +1,7 @@
 (ns yorick-demo.inputs
   (:require [theophilusx.yorick.card :as c]
-            [theophilusx.yorick.input :as i]))
+            [theophilusx.yorick.input :as i]
+            [reagent.core :as r]))
 
 (defn field-component []
   [:div.columns
@@ -79,6 +80,75 @@
                 :label "has-text-warning"
                 :body "has-background-primary"}]]]])
 
+(defn input-component []
+  [:div.columns
+   [:div.column.is-half
+    [c/card
+     [:<>
+      [:p
+       "A basic text input component. This component is typically wrapped "
+       "inside a " [:em "field"] " or " [:em "horizontal-field"] " component. "
+       "The " [:code "type"] " argument specifies the type of input field. The "
+       "supported types are keywords derived from the HTML5 input types e.g. "
+       [:code ":text"] ", " [:code ":email"] ", " [:code ":password"] ", etc."
+       "The " [:code ":sid"] " argument specifies a storage identifier for the "
+       "user input. A storage identifier is a " [:em "keyword"] " that specifies "
+       "where the user input data will be stored within the document model "
+       "atom. Any period in the keyword will be treated as a path separator to "
+       "create key hierarchies for storage e.g. " [:code ":value"] ", "
+       [:code ":my-form.value"] "."]
+      [:p
+       "The " [:code "input"] " component also supports a number of optional "
+       "keyword arguments"]
+      [:ul
+       [:li [:strong ":model"] " - a reagent atom used as the document model "
+        "for storage"]
+       [:li [:strong "change-fn"] " - a function of one argument which will be "
+        "called when the input data changes. Manages the storage of input "
+        "data in the document model atom"]
+       [:li [:strong ":classes"] " - a map of CSS class names to add to HTML "
+        "elements which make up the component. Each value is either a string "
+        "representing a CSS class name or a vector of strings representing "
+        "CSS class names. Supported keys are " [:code ":control"] " and "
+        [:code ":input"] "."]
+       [:li [:strong ":icon-data"] " - either an icon-data map or a vector of "
+        "icon-data maps. See " [:strong "theophilusx.yorick.icon"] " for "
+        "details on the " [:code "icon-data"] " map."]
+       [:li [:strong ":placeholder"] " - placeholder text to use in the input"]
+       [:li [:strong ":required"] " - set HTML required attribute (boolean)"]
+       [:li [:strong ":disabled"] " - set HTML disabled attribute (boolean)"]
+       [:li [:strong ":min"] " - set the HTML min attribute (number)"]
+       [:li [:strong ":max"] " - set the HTML max attribute (number)"]
+       [:li [:strong ":minlength"] " - set the HTML minLength attribute (number)"]
+       [:li [:strong ":maxlength"] " - set the HTML maxLength attribute (number)"]
+       [:li [:strong ":readonly"] " - set the HTML readonly attribute (boolean)"]
+       [:li [:strong ":size"] " - set the HTML size attribute (number)"]]]
+     :header {:title "input - general purpose input component"}]]
+   [:div.column
+    [:pre
+     [:code
+      "[:p \"A basic text input field\"]" [:br]
+      "[input/field [input/input :text :value]]" [:br]
+      "(let [doc (reagent/atom {})]" [:br]
+      "      eg-fn (fn []" [:br]
+      "              [:<>" [:br]
+      "                [:p \"Basic password input field example\"]" [:br]
+      "                [input/field [input/input :password :value :model doc" [:br]
+      "                              :placeholder \"enter password\"] :label \"Password:\"]" [:br]
+      "                [:p (str \"Value entered: \" (:value @doc))]])]" [:br]
+      "  [eg-fn])"]]
+    [:div.box
+     [:p "A basic text input field"]
+     [i/field [i/input :text :value]]
+     (let [doc (r/atom {})
+           eg-fn (fn []
+                   [:<>
+                    [:p "Basic password input field example"]
+                    [i/field [i/input :password :value :model doc
+                              :placeholder "enter password"] :lable "Password:"]
+                    [:p (str "Value entered: " (:value @doc))]])]
+       [eg-fn])]]])
+
 (defn input-page []
   [:<>
    [:h2.title.is-2 "Input Components"]
@@ -98,11 +168,40 @@
     "this namespace ensure input fields meet these requirements and provide "
     "some higher level convenience components for common or useful input types."]
    [:hr]
+   [:h4.title.is-4 "Document Models and Storage identifiers"]
+   [:p
+    "Input data obtained from the user must be stored somewhere to be of use. "
+    "The components in " [:em "Yorick"] " use reagent atoms for this storage. "
+    "This library uses the term " [:em "document model"] " to refer to the "
+    "Reagent atoms used to store user input. A " [:em "document model"] " is an "
+    "atom witht the shape of a Clojurescript " [:em "map"] ". This means that "
+    "multiple values can be stored within one document model (for example, all "
+    "the inputs from a form can be stored in one document model). To control "
+    "where data is stored within a document model, the concept of "
+    [:em "storage identifiers"] " or " [:em "sid"] " is introduced. A "
+    [:em "sid"] " is a " [:em "keyword"] " which specifies the path into a "
+    "document model map atom where the data should be stored. To support key "
+    "hierarchies in the map, the " [:em "sid"] " uses a period (.) as a path "
+    "separator. For example, a sid with the value " [:code ":value"] " will "
+    "store the input in the document model map under the key " [:em ":value"]
+    " and a sid with the value " [:code ":my-form.value"] " will store the "
+    "input in the document model map under the key " [:em ":value"] " which is "
+    "under the key " [:em ":my-form"] "."]
+   [:p
+    "Most of the components in this namespace which accept user input have a "
+    "local document model reagent atom which will be used if none is specified "
+    "with the `:model` keyword argument. You can also specify a change function "
+    "of one argument for more complex storage requirements. The `:change-fn` "
+    "optional argument allows you to specify the function that will be called "
+    "whenever the input data changes. The argument represents the new input "
+    "value."]
+   [:hr]
    [:div.columns.is-half
     [:div.column
      [:h4.title.is-4 "Description"]]
     [:div.column
      [:h4.title.is-4 "Example"]]]
    [field-component]
-   [horizontal-field-component]])
+   [horizontal-field-component]
+   [input-component]])
 

4346b738e76542e06b107ba2834253920dbe961d
Tim Cross <theophilusx@gmail.com>
2020-07-18 11:23:51 +1000

    Add example of deftab use to demo app

--- demo/yorick_demo/tabs.cljs
+++ demo/yorick_demo/tabs.cljs
@@ -2,7 +2,8 @@
   (:require [theophilusx.yorick.tab :as t]
             [theophilusx.yorick.utils :refer [spath]]
             [theophilusx.yorick.card :as c]
-            [theophilusx.yorick.store :refer [get-in global-state]]))
+            [theophilusx.yorick.store :refer [get-in global-state]]
+            [theophilusx.yorick.basic :as b]))
 
 (defn deftab-function []
   [:div.columns
@@ -11,15 +12,40 @@
     [c/card
      [:div.content
       [:p
-       "Description of deftab"]]
+       "The " [:strong "deftab"] " function is a convenience function for "
+       "defining tab elements for a horizontal navigation bar. The tab "
+       "definitions are a map with specific keys. The " [:code "title"]
+       " argument is the text to appear in the tab. The function supports the "
+       "following optional keyword arguments:"]
+      [:ul
+       [:li [:strong ":id"] " - a unique value to be used as the id for a tab. "
+        "When a tab is selected, this value is set in the global document model "
+        "store under the storage identifier key associated with the tab bar. "
+        "If not specified, the " [:code "title"] " argument, converted to a "
+        "keyword is used as the default."]
+       [:li [:strong ":icon-data"] " - an icon data map defining an icon to be "
+        "added to the tab. See " [:code "theophilusx.yorick.icon"] " for "
+        "details on the structure of icon data maps."]
+       [:li [:strong ":class"] " - a string or vector of strings specifying "
+        "CSS class names to be added to the tab."]]]
      :header {:title "deftab - convenience function for defining tabs"}]]
    [:div.column
     [:h4.title.is-4 "Example"]
     [:pre
      [:code
-      "Example Code"]]
+      "(let [tabs [(t/deftab \"Tab 1\" :id :tab1)" [:br]
+      "            (t/deftab \"Tab 2\" :id :tab2)" [:br]
+      "            (t/deftab \"Tab 3\" :id :tab3)]]" [:br]
+      "  [:<>" [:br]
+      "    [:p \"Example tab definitions\"]" [:br]
+      "    [b/render-vec tabs]])"]]
     [:div.box
-     "Example"]]])
+     (let [tabs [(t/deftab "Tab 1" :id :tab1)
+                 (t/deftab "Tab 2" :id :tab2)
+                 (t/deftab "Tab 3" :id :tab3)]]
+       [:<>
+        [:p "Example tab definitions"]
+        [b/render-vec tabs]])]]])
 
 (defn tab-component []
   [:div.columns

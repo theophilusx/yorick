a5849b885183aaa5352dd20e6ad30b9d32b0d934
Tim Cross <theophilusx@gmail.com>
2020-07-22 10:46:37 +1000

    Add tabs to the card demo page

--- demo/yorick_demo/cards.cljs
+++ demo/yorick_demo/cards.cljs
@@ -1,7 +1,10 @@
 (ns yorick-demo.cards
   (:require [theophilusx.yorick.card :as c]
             [theophilusx.yorick.basic :as b]
-            [theophilusx.yorick.icon :as icon]))
+            [theophilusx.yorick.icon :as icon]
+            [theophilusx.yorick.tab :as t]
+            [theophilusx.yorick.store :refer [get-in global-state]]
+            [theophilusx.yorick.utils :refer [spath]]))
 
 (defn simple-card []
   [:div.columns
@@ -143,38 +146,38 @@
        :classes {:card-content "has-text-danger"}]]]]])
 
 (defn card-page []
-  [:div.content
-   [:h2.title.is-2 "The Card Component"]
-   [:p
-    "The " [:strong "theophilusx.yorick.card/card"] " component provides a flexible "
-    "and composable component for presenting almost any type of content. The "
-    "card consists of 3 parts, a header, a footer and the body. Only the body "
-    "is required, header and footer are optional."]
-   [:p
-    "The body contents of a card can be almost anything - a string, hiccup "
-    "markup, another component or a combination. The " [:em "body"] " argument "
-    "needs to be in a form suitable as the contents of a Reagent/React "
-    "component i.e. it cannot be a nested vector or list. If you want to pass "
-    "in multiple components or vectors of hiccup markup, you need to either "
-    "wrap the argument in a " [:em "div"] " vector or use the " [:em "[:<>]"]
-    "shorthand wrapper."]
-   [:p
-    "The optional " [:em ":header"] " argument expects a map value. Supported "
-    "keys for the map are " [:em ":title, :icon, :icon-action"] " and "
-    [:em ":class"] "."]
-   [:ul.list
-    [:li [:strong ":title"] " Text of the header title"]
-    [:li [:strong ":icon"] " An icon definition map. See "
-     [:strong "theophilusx.yorick.icon"] " for details on the map format"]
-    [:li [:strong ":icon-action"] " An optional function to execute if the "
-     "user clicks on the icon"]
-    [:li [:strong ":class"] " A string or vector of strings representing "
-     "CSS class names to add to the header"]]
+  [:<>
+   [:div.content
+    [:h2.title.is-2 "The Card Component"]
+    [:p
+     "The " [:strong "theophilusx.yorick.card/card"] " component provides a flexible "
+     "and composable component for presenting almost any type of content. The "
+     "card consists of 3 parts, a header, a footer and the body. Only the body "
+     "is required, header and footer are optional."]
+    [:p
+     "The body contents of a card can be almost anything - a string, hiccup "
+     "markup, another component or a combination. The " [:em "body"] " argument "
+     "needs to be in a form suitable as the contents of a Reagent/React "
+     "component i.e. it cannot be a nested vector or list. If you want to pass "
+     "in multiple components or vectors of hiccup markup, you need to either "
+     "wrap the argument in a " [:em "div"] " vector or use the " [:em "[:<>]"]
+     "shorthand wrapper."]
+    [:p
+     "The optional " [:em ":header"] " argument expects a map value. Supported "
+     "keys for the map are " [:em ":title, :icon, :icon-action"] " and "
+     [:em ":class"] "."]
+    [:ul.list
+     [:li [:strong ":title"] " Text of the header title"]
+     [:li [:strong ":icon"] " An icon definition map. See "
+      [:strong "theophilusx.yorick.icon"] " for details on the map format"]
+     [:li [:strong ":icon-action"] " An optional function to execute if the "
+      "user clicks on the icon"]
+     [:li [:strong ":class"] " A string or vector of strings representing "
+      "CSS class names to add to the header"]]]
    [:hr]
-   [:div.columns
-    [:div.column.is-half
-     [:h4.title.is-4 "Description"]]
-    [:div.column
-     [:h4.title.is-4 "Examples"]]]
-   [simple-card]])
+   [t/tab :ui.tabs.card-page [(t/deftab "card" :id :card)] :size :large
+    :position :center]
+   (case (get-in global-state (spath :ui.tabs.card-page))
+     :card [simple-card]
+     [simple-card])])
 

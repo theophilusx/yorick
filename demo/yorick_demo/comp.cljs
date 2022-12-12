477b5c85f54ab9a5d0856af8f38ead487de1e8ab
Tim Cross <theophilusx@gmail.com>
2022-12-12 16:29:02 +1100

    Add comp namespace
    Add some basic functions to assist in formatting examples and documentation for
    the demo app.

--- demo/yorick_demo/comp.cljs
+++ demo/yorick_demo/comp.cljs
@@ -6,12 +6,23 @@
         (for [[arg doc] arg-list]
           [:li [:strong arg] (str " " doc)])))
 
-(defn doc-comp [title doc args]
-  [c/card [:div.content
-           doc
-           (when args
-             [doc-args args])]
-   :header {:title title :class ["has-background-info" "has-text-white"]}])
+(defn doc-comp
+  ([title doc]
+   (doc-comp title doc nil nil))
+  ([title doc args]
+   (doc-comp title doc args nil))
+  ([title doc args opt-args]
+   [c/card [:div.content
+            doc
+            (when args
+              [:<>
+               [:h6.title.is-6 "Arguments"]
+               [doc-args args]])
+            (when opt-args
+              [:<>
+               [:h6.title.is-6 "Optional Keyword Arguments"]
+               [doc-args opt-args]])]
+    :header {:title title :class ["has-background-info" "has-text-white"]}]))
 
 (defn example [ex comp]
   [c/card

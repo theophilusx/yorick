9a2c95b3a4328b117ec340de562edb45e508800a
Tim Cross <theophilusx@gmail.com>
2023-02-26 16:40:44 +1100

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject theophilusx/yorick "1.0.2"
+(defproject theophilusx/yorick "2.0.0"
   :description "A library of Reagent components styled using Bulma"
   :url "https://github.com/theophilusx/yorick"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.11.60"]
-                 [reagent "1.1.1"]]
+                 [reagent "1.2.0"]]
   :source-paths ["src"]
   :plugins [[lein-codox "0.10.8"]]
   :codox {:source-url "https://github.com/theophilusx/yorick/blob/master/{filepath}#{line}"
